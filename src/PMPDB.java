2f4e2ab3a3ca38ae7a712c1ad900f015717ccb53
rjdangelo <dangelo.rj@gmail.com>
2017-08-03 08:32:33 -0400

    Field Separator change
    - changed field separator from ; to | to avoid malformed csv files
    - replaced %empty% indicator with null string

--- src/PMPDB.java
+++ src/PMPDB.java
@@ -28,18 +28,18 @@ public class PMPDB {
 	private static final String HELP = "read all the PMP files containing the Picasa Database and the file " +
 	                		"containing indexes thumbindex.db. \nThis program will create 3 csv files: albumdata.csv " +
 	                		"(album database), catdata.csv (category database), imagedata.csv (picture database).\n\nParameters:\n";
-	
+
 	HashMap<String, ArrayList<String>> catdata;
 	HashMap<String, ArrayList<String>> albumdata;
 	HashMap<String, ArrayList<String>> imagedata;
 	String folder;
 	Indexes indexes;
-	
+
 	public PMPDB(String folder) {
 		this.folder = folder;
 		indexes = new Indexes(folder);
 	}
-	
+
 	public void populate() throws Exception{
 		indexes.Populate();
 		catdata = getTable("catdata");
@@ -56,35 +56,35 @@ public class PMPDB {
 		imagedata.put("index",is);
 		imagedata.put("original index", ois);
 		imagedata.put("name", indexes.names);
-		
+
 	}
-	
-	
+
+
 	private HashMap<String, ArrayList<String>>  getTable(final String table) throws Exception{
         //filter on table_*.pmp
-        FilenameFilter filter = new FilenameFilter() { 
+        FilenameFilter filter = new FilenameFilter() {
             @Override
             public boolean accept(File dir, String filename)
-            { 
+            {
             	return filename.startsWith(table+"_") && filename.endsWith(".pmp");
             }
         };
-        
+
         File[] files = new File(folder).listFiles(filter);
         HashMap<String, ArrayList<String>> data = new HashMap<String, ArrayList<String>>();
-        
+
         for(int i=0; i<files.length; i++){
             String filename 	= files[i].getName();
 		    String fieldname	= filename.replace(table+"_", "").replace(".pmp", "");
-	            
+
 		    //System.out.print(fieldname+" ");
             data.put(fieldname, readColumn(folder+filename)); //saving column fieldname
         }
-        
-        
+
+
         return data;
     }
-	
+
 	private static ArrayList<String> readColumn(String file) throws Exception{
 		ArrayList<String> l = new ArrayList<String>();
         DataInputStream din = new DataInputStream
@@ -92,7 +92,7 @@ public class PMPDB {
              (new FileInputStream(file)));
         long magic = ReadFunctions.readUnsignedInt(din); //file start with a magic sequence
         int type = ReadFunctions.readUnsignedShort(din); // then the entries type
-         if ((magic=ReadFunctions.readUnsignedShort(din)) != 0x1332) {  //first constant
+        if ((magic=ReadFunctions.readUnsignedShort(din)) != 0x1332) {  //first constant
             throw new IOException("Failed magic2 "+Long.toString(magic,16));
         }
         if ((magic=ReadFunctions.readUnsignedInt(din)) != 0x2) {  //second constant
@@ -106,12 +106,12 @@ public class PMPDB {
             throw new IOException("Failed magic4 "+Long.toString(magic,16));
         }
         long v = ReadFunctions.readUnsignedInt(din); //number of entries
-        
-        
+
+
         // records.
         for(int i=0; i<v; i++){
             if (type == 0) {
-                l.add(ReadFunctions.dumpStringField(din));
+                l.add("\""+ReadFunctions.dumpStringField(din)+"\"");
             }
             else if (type == 0x1) {
                 l.add(ReadFunctions.dump4byteField(din));
@@ -141,13 +141,13 @@ public class PMPDB {
         din.close();
         return l;
     }
-	
+
 	public void writeCSVs(String output) throws Exception{
 		writeCSV("catdata", catdata, output);
 		writeCSV("imagedata", imagedata, output);
 		writeCSV("albumdata", albumdata, output);
 	}
-	
+
     private static void writeCSV(String table, HashMap<String, ArrayList<String>> data, String output) throws Exception{
     	// not all files have the same number of elements, get the maximum size for a table
         int max = 0;
@@ -157,27 +157,27 @@ public class PMPDB {
             if(size>max)
                 max=size;
         }
-        
+
         StringBuilder s = new StringBuilder();
-        
+
         // column names
         for (String key:data.keySet()){
                 s.append(key);
-                s.append(";");
+                s.append("|");
             }
         s.append("\n");
-        
-        
+
+
         for(int i=0; i<max ; i++){
             for (String key:data.keySet()){
-                
+
                 // for column that have less elements that the max, fill the end with %empty%
                 if(data.get(key).size()>i){
                     s.append(data.get(key).get(i));
                 }else{
-                    s.append("%empty%");
+                    s.append("");
                 }
-                s.append(";");
+                s.append("|");
             }
             s.append("\n");
         }
@@ -186,14 +186,14 @@ public class PMPDB {
         bw.write(s.toString());
         bw.close();
     }
-	
+
 	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws Exception {
 		Options options = new Options();
     	options.addOption("h","help", false, "prints the help content");
     	options.addOption(OptionBuilder.withArgName("srcFolder").hasArg().withDescription("Picasa DB folder. Default is " + EnvironmentVariables.DEFAULT_PICASA_DB_PATH).create(PARAM_PICASA_DB_FOLDER));
     	options.addOption(OptionBuilder.withArgName("outputFolder").hasArg().isRequired().withDescription("output folder").create(PARAM_OUTPUT_FOLDER));
-    	
+
     	CommandLineParser parser = new GnuParser();
     	String folder=null;
     	String output=null;
@@ -204,7 +204,7 @@ public class PMPDB {
             	showHelp(options);
                 System.exit(1);
             }
-            
+
             folder = EnvironmentVariables.getPicasaDBFolder(line, PARAM_PICASA_DB_FOLDER);
 
             if(line.hasOption(PARAM_OUTPUT_FOLDER)){
@@ -219,12 +219,12 @@ public class PMPDB {
         }
         catch( ParseException exp ) {
             // oops, something went wrong
-        	
+
             System.err.println( "Parsing failed.  Reason: " + exp.getMessage() );
             showHelp(options);
             System.exit(1);
         }
-        
+
         PMPDB db = new PMPDB(folder);
         db.populate();
         db.writeCSVs(output);
@@ -232,8 +232,8 @@ public class PMPDB {
 
 	private static void showHelp(Options options) {
 		HelpFormatter formatter = new HelpFormatter();
-		formatter.printHelp(APPTITLE, 
-				HELP, 
+		formatter.printHelp(APPTITLE,
+				HELP,
 				options, "\n", true);
 	}
 }
